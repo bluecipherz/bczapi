6b850fa6b3eda5c2f181f78df25c824f42d36eaf
Mohammed Basith <basith374@gmail.com>
2015-10-05 00:27:10 +0530

    mm

--- app/Handlers/Events/DeleteFeed.php
+++ app/Handlers/Events/DeleteFeed.php
@@ -31,26 +31,41 @@ class DeleteFeed {
 		// $lastFeed : Comment Feed
 		if($event->getType() == 'CommentDeleted') {
 			// echo 'ok1';
-			$lastFeed = Feed::whereType('CommentPosted')->whereContextId($event->getContext()->id)->first(); // context = target feed
-			if($lastFeed) {
-				// echo "ok2;{$event->getContext()->comments->count()}";
-				if($event->getContext()->comments->count() > 0) {
-					$lastComment = $event->getContext()->comments->last();
-					// echo "count:{$event->getContext()->comments->count()};last:{$lastComment->owner}";
-					$lastFeed->subject()->associate($lastComment);
-					$lastFeed->origin()->associate($lastComment->owner);
-					$lastFeed->updated_at = $lastComment->updated_at;
-					$lastFeed->save();
-					// echo "{$lastFeed} : {$lastComment}";
-				} else { // delete comment feed
-					// $event->getContext()->delete();
-					$lastFeed->delete();
-					// echo 'comment feed deleted';
+			// $lastFeed = Feed::whereType('CommentPosted')->whereContextId($event->getContext()->id)->first(); // context = target feed
+			$feed = $event->getContext();
+			if($feed->additional_type == 'CommentPosted') {
+				if($feed->comments->count() > 0) {
+					$lastComment = $feed->comments->last();
+					// $feed->additional_subject_type = 'App\Comment';
+					$feed->additional_subject_id = $event->getSubject()->id;
+					$feed->updated_at = $lastComment->updated_at;
+					$feed->save();
+				} else {
+					$feed->additional_type = '';
+					$feed->additional_subject_id = 0;
+					$feed->additional_subject_type = '';
+					$feed->save();
 				}
 			}
+			// if($lastFeed) {
+				// echo "ok2;{$event->getContext()->comments->count()}";
+				// if($event->getContext()->comments->count() > 0) {
+					// $lastComment = $event->getContext()->comments->last();
+					// echo "count:{$event->getContext()->comments->count()};last:{$lastComment->owner}";
+					// $lastFeed->subject()->associate($lastComment);
+					// $lastFeed->origin()->associate($lastComment->owner);
+					// $lastFeed->updated_at = $lastComment->updated_at;
+					// $lastFeed->save();
+					// echo "{$lastFeed} : {$lastComment}";
+				// } else { // delete comment feed
+					// $event->getContext()->delete();
+					// $lastFeed->delete();
+					// echo 'comment feed deleted';
+				// }
+			// }
 		} else if($event->getType() == 'ProjectDeleted') {
             Feed::whereProjectId($event->getSubject()->id)->delete(); // delete all project related feeds
-            Feed::whereSubjectId($event->getSubject()->id)->whereSubjectType(get_class($event->getSubject()))->delete(); // create 'project created' feed
+            Feed::whereSubjectId($event->getSubject()->id)->whereSubjectType('App\Project')->delete(); // create 'project created' feed
 		} else {
             Feed::whereSubjectId($event->getSubject()->id)->delete();
         }
